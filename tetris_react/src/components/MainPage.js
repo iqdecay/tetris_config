f915b3a15c59c3bf874a6f93a53f93dda147fe90
vnepveu <victor.nepveu@imt-atlantique.net>
2020-11-24 13:26:50 +0100

    Add API call to fetch info on last ACK response
    - Add GET api call to fetch info on all devices
    - Rework data model to accomodate go server structs
    - Rework StatusInfo display

--- tetris_react/src/components/MainPage.js
+++ tetris_react/src/components/MainPage.js
@@ -1,7 +1,6 @@
 import React from "react"
 import BeaconItem from "./BeaconItem"
 import Navbar from "./Navbar"
-import beaconData from "../beaconData"
 import "../style.css"
 
 
@@ -9,9 +8,38 @@ class MainPage extends React.Component {
     constructor() {
         super();
         this.state = {
-            beacons: beaconData
+            beacons: {},
+            apiCallReturned: false
         }
         this.handleChange = this.handleChange.bind(this)
+    }
+
+    componentDidMount() {
+        const url = "/info/"
+        fetch(url, {
+            method: 'GET',
+            headers: {
+                "Content-Type": "application/json",
+                'Accept': 'application/json',
+            }
+        })
+            .then(response => response.json())
+            .then(data => {
+                    this.setState({beacons: data})
+                }
+            )
+            .then(data => this.setState({apiCallReturned: true}))
+            .then(data => console.log("Info call state : ", this.state.beacons))
+            .catch(err => console.log(err))
+    }
+
+    generateBeaconsList() {
+        return Object.keys(this.state.beacons).map((key, index) => {
+                return <BeaconItem key={key} id={key}
+                                   beacon={this.state.beacons[key]}
+                                   handleChange={this.handleChange}/>
+            }
+        )
     }
 
     handleChange(id) {
@@ -29,19 +57,17 @@ class MainPage extends React.Component {
                 beacons: updatedBeacons
             }
         })
-        console.log("Changed !", id)
     }
 
 
     render() {
-        const beaconsList = this.state.beacons.map(
-            item => <BeaconItem key={item.id} beacon={item}
-                                handleChange={this.handleChange}/>);
+        const renderBeaconsList = this.state.apiCallReturned ?
+            this.generateBeaconsList() : <h1> Loading ...</h1>
         return (
             <div>
                 <Navbar title="Sigfox supervision des balises"/>
                 <div className="centered-box">
-                    {beaconsList}
+                    {renderBeaconsList}
                 </div>
             </div>
         )
